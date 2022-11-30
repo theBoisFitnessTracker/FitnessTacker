0c63f400552018f9aa52d1a1cf9f0fe637cd5917
Dale Haase <dalehaase09@gmail.com>
2022-11-30 18:48:19 -0500

    encrpted passwords

--- api/Users.js
+++ api/Users.js
@@ -97,4 +97,20 @@ usersRouter.post("/register", async (req, res, next) => {
   }
 });
 
+// calling an async function after the create user function to hash and encrypt the password
+async function hashedPassword (password){
+  try {
+      // assigning saltValue to an await function to then generate the salt value
+      const saltValue = await bcrypt.genSalt(8)
+      console.log(`I am the salt value: ${saltValue}`)
+      // now we are hashing our salt value by passing password and saltValue as a promise 
+      const hashedValue = await bcrypt.hash(password, saltValue)
+      console.log(`I am the hashed value: `, hashedValue)
+  } catch (error) {
+      console.log(error)
+  }
+};
+// callling the hashedPassword function to show in our terminal/console
+hashedPassword('password');
+
 module.exports = usersRouter;
