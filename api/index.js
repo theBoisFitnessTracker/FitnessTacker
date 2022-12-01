438030edc92ed096531fbf56cc4bc6dd07c0ff49
Dale Haase <dalehaase09@gmail.com>
2022-11-30 19:38:47 -0500

    express server listening

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 // api subrouter goes here
-
+const express = require('express');
+const apiRouter = express.Router()
 // routineActivities subrouter use here
 // routine subrouter use here
 // users subrouter use here
 // activities subrouter use here
+
+
+
+
+
+module.exports = apiRouter
