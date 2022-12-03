9f25c2ddd6005f0554c398d070d3e5f84822ce31
kgrif113 <91581102+kgrif113@users.noreply.github.com>
2022-12-03 02:13:43 -0500

    express routes

--- api/Activities.js
+++ api/Activities.js
@@ -1,17 +1,107 @@
 // create subrouter here
+const {
+  createActivities,
+  getAllActivities,
+  getActivitesById,
+  updateActivities,
+} = require('../db')
 const express = require("express");
 const activitiesRouter = express.Router();
 // export subrouter below here
 
 // Kenny: build out express get, post, patch, delete routes
-activitiesRouter.get("/", (req, res, next) => {});
+activitiesRouter.get("/", async (req, res, next) => {
+  try {
+    const activities = await getAllActivities();
+    if (activities) {
+      res.status(200).send(activities);
+    } else {
+      const error = {
+        name: "cannotGetActivities",
+        message: "Unable to get activities, try again later",
+      };
+      res.status(400).send(error);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
-activitiesRouter.get('/:activityId', (req, res, next) => {})
+activitiesRouter.get("/:activityId", async (req, res, next) => {
+  try {
+    // need to get activityID from params
+    // need to input activityID into "get" function
+    const activityById = await getActivitesById({id});
+    if (activityById) {
+      res.status(200).send(activityById);
+    } else {
+      const error = {
+        name: "cannotGetActivity",
+        message: "Unable to get activity, try again later",
+      };
+      res.status(400).send(error);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 // posts
+activitiesRouter.post("/:activities", async (req, res, next) => {
+  try {
+    // need to use 
+    const createActivity = await createActivities({name, description});
+    if (createActivity) {
+      res.status(200).send(createdActivity);
+    } else {
+      const error = {
+        name: "cannotPostActivity",
+        message: "Unable to create activity, try again later",
+      };
+      res.status(400).send(error);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 // patches
+activitiesRouter.patch("/:activityId", async (req, res, next) => {
+  try {
+    const updateActivity = await updateActivities(id, {...fields});
+    if (updateActivity) {
+      res.status(200).send(updatedActivtiy);
+    } else {
+      const error = {
+        name: "cannotUpdateActivity",
+        message: "Cannot update activity, try again later",
+      };
+      res.status(400).send(error);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 // deletes
+activitiesRouter.delete("/:activityId", async (req, res, next) => {
+  try {
+    // get id from params
+    // delete activity using id
+    // do not have a delete function written yet.
+   const deletedActivity= await deleteActivity();
+    if (deletedActivity) {
+      res.status(200).send(deletedActivity);
+    } else {
+      const error = {
+        name: "cannotDeleteActivity",
+        message: "Cannot delete Activity, try again later",
+      };
+      res.status(400).send(error);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 module.exports = activitiesRouter;
--- api/RoutineActivities.js
+++ api/RoutineActivities.js
@@ -1,15 +1,103 @@
 // create subrouter here
+const {
+  getRoutineActivityById,
+  addActivityToRoutine,
+  getRoutineActivitiesByRoutine,
+  updateRoutineActivity,
+} = require("../db");
 const express = require("express");
 const raRouter = express.Router();
 // export subrouter below here
 // Kenny: build out express get, post, patch, delete routes
 
 // gets
+raRouter.get("/:raId", async (req, res, next) => {
+  try {
+    const id = req.params.raId;
+    // need to get routine_activity by ID - raId
+    // need to use raId in the function
+    const routineActivity = await getRoutineActivityById({ id });
+    if (routineActivity) {
+      res.status(200).send(routineActivity);
+    } else {
+      const error = {
+        name: "cannotGetActivity",
+        message: "Unable to get activity, try again later",
+      };
+      res.status(400).send(error);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+raRouter.get("/:routineId", async (req, res, next) => {
+  try {
+    const routineId = req.params.routineId;
+    const routineActivity = await getRoutineActivitiesByRoutine({ routineId });
+    if (routineActivity) {
+      res.status(200).send(routineActivity);
+    } else {
+      const error = {
+        name: "cannotGetRoutineActivity",
+        message: "Unable to get routine activity, try again later",
+      };
+      res.status(400).send(error);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 // posts
+raRouter.post("/:routineId", async (req, res, next) => {
+  try {
+    const routineId = req.params.routineId;
+    const { activityId, duration, count } = req.body;
+    const acitvity = await addActivityToRoutine({
+      routineId,
+      activityId,
+      duration,
+      count,
+    });
+    if (acitvity) {
+      res.status(200).send(acitvity);
+    } else {
+      const error = {
+        name: "cannotAddActivity",
+        message: "Unable to add activity, try again later",
+      };
+      res.status(400).send(error);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 // patches
+raRouter.patch("/:raId", async (req, res, next) => {
+  try {
+    const routineId = req.params.raId;
+    const routineActivity = await updateRoutineActivity(id, {...req.body });
+    if (routineActivity) {
+      res.status(200).send(routineActivity);
+    } else {
+      const error = {
+        name: "cannotUpdateRoutineActivity",
+        message: "Unable to update activity, try again later",
+      };
+      res.status(400).send(error);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 // deletes
+// raRouter.delete("/:raId", async (req, res, next) => {
+//   try {
+//     const id = ??
+//   } catch (error) {}
+// });
 
 module.exports = raRouter;
